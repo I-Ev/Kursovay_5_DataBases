70de8034f06b949e9b1f81e70e7134c79225779c
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-06-23 19:43:39 +0300

    changes

--- hh_utils.py
+++ hh_utils.py
@@ -1,3 +1,4 @@
+import random
 from typing import Any
 import psycopg2
 import requests
@@ -42,19 +43,55 @@ def get_companies_from_hh(area_id: str) -> list[dict[str, Any]]:
 def create_db(db_name: str, params: dict) -> None:
     """ Создает БД и таблицы для хранения информации с сайта HH.ru"""
     conn = psycopg2.connect(dbname='postgres', **params)
-    conn.autocommit = True
+    conn.autocommit = True  # установка autocommit в True
     cur = conn.cursor()
-
-    cur.execute(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{db_name}'")
-    exists = cur.fetchone()
-    if not exists:
+    try:
         cur.execute(f'CREATE DATABASE {db_name}')
+    except psycopg2.ProgrammingError:
+        pass
 
     cur.close()
     conn.close()
 
+    # try:
+    #     conn = psycopg2.connect(dbname=db_name, **params)
+    # except psycopg2.OperationalError as e:
+    #     print(f"Unable to connect to database: {e}")
+    # else:
+    #     with conn.cursor() as cur:
+    #         cur.execute("""
+    #             CREATE TABLE IF NOT EXISTS companies (
+    #                 company_id SERIAL PRIMARY KEY,
+    #                 company_name VARCHAR(255) NOT NULL,
+    #                 url TEXT
+    #             )
+    #         """)
+    #     conn.commit()
+    # finally:
+    #     conn.close()
+
+    with psycopg2.connect(dbname=db_name, **params) as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS companies (
+                    company_id SERIAL PRIMARY KEY,
+                    company_name VARCHAR(255) NOT NULL,
+                    url TEXT
+                )
+            """)
 
 
 def save_10random_companies_to_db(data: list[dict[str, Any]], db_name: str, params: dict) -> None:
     """Сохраняет 10 случайных компаний в БД"""
-    return None
+    with psycopg2.connect(dbname=db_name, **params) as conn:
+        with conn.cursor() as cur:
+            for company in data:
+                cur.execute("""
+                    INSERT INTO companies (company_id, company_name, url)
+                    VALUES (%s, %s, %s)
+                    """, (company['id'], company['name'], company['url']))
+            conn.commit()
+
+
+def get_10random_companies_data(companies_data: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    return random.sample(companies_data, 10)
--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from config import config
 def main():
     params = config()
 
-    # utils.get_companies_from_hh('63')
-    utils.create_db('HeadHunter', params)
-    # utils.save_10random_companies_to_db(data, 'HeadHunter', params)
+    data = utils.get_companies_from_hh('63')
+    random_10_companies = utils.get_10random_companies_data(data)
+    utils.create_db('headhunter', params)
+    utils.save_10random_companies_to_db(random_10_companies, 'headhunter', params)
 
-    # get_vacancies_from_hh()
-    # save_vacancies_to_db()
+    vacancies = utils.get_vacancies_from_hh(random_10_companies)
+    utils.save_vacancies_to_db(vacancies)
 
 
 
